8c3d8fb2704d92ef00002d2c6061457b8fd73428
MattB400 <brock.m@wustl.edu>
2025-08-27 15:32:07 -0500

    config file updated

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,9 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" WashU Bears ");
+		System.out.println(" WashU Bears 123912301");
+	//comment
+	
+	
 	}   
 }
